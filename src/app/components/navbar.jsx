2c0ec425346d7058d7274bd82bedf80931d33e09
cloudflips32 <devadamstandish@gmail.com>
2025-06-22 01:31:22 -0400

    navbar adjustments

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-black light:bg-white shadow-sm">
+    <nav className="bg-black light:bg-white shadow-sm w-[auto]">
       <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center h-30">
+        <div className="flex justify-between items-center h-30 mx-auto">
           <Link href="/" className="flex items-center">
             <div className="w-40 h-10 rounded-full bg-purple-700 flex items-center hover:scale-102 justify-center mr-3 opacity-80">
               <img src="bsideslogo.png" alt="bsides swfl logo" />
@@ -23,35 +23,35 @@ const Navbar = () => {
 
           <div className="hidden md:flex items-center space-x-6">
             <Link href="/aboutus" className="light:text-black text-orange-200 hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex"><ScrollIcon size={20} className="mr-2 blur-[0.75px]" />About Us</p>
+              <p className="my-4 inline-flex"><ScrollIcon size={20} className="mr-2 blur-[0.75px]" />About Us</p>
             </Link>
 
             <Link href="/events" className="light:text-black text-orange-200 hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex"><Computer size={20} className="mr-2 blur-[0.75px]" />Events</p>
+              <p className="my-4 inline-flex"><Computer size={20} className="mr-2 blur-[0.75px]" />Events</p>
             </Link>
 
             <Link href="/schedule" className="light:text-black text-orange-200 hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex"><Calendar size={20} className="mr-2 blur-[0.75px]" />Schedule</p>
+              <p className="my-4 inline-flex"><Calendar size={20} className="mr-2 blur-[0.75px]" />Schedule</p>
             </Link>
 
             <Link href="/speakers" className="light:text-black text-orange-200 hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex"><User2 size={20} className="mr-2 blur-[0.75px]" />Speakers</p>
+              <p className="my-4 inline-flex"><User2 size={20} className="mr-2 blur-[0.75px]" />Speakers</p>
             </Link>
 
             <Link href="/workshops" className="light:text-black hidden text-orange-200 hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex">Workshops</p>
+              <p className="my-4 inline-flex">Workshops</p>
             </Link>
 
             <Link href="/sponsors" className="light:text-black text-orange-200 hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex"><Ribbon size={20} className="mr-2 blur-[0.75px]" />Sponsors</p>
+              <p className="my-4 inline-flex"><Ribbon size={20} className="mr-2 blur-[0.75px]" />Sponsors</p>
             </Link>
 
             <Link href="/volunteer" className="light:text-black text-orange-200 hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex"><Handshake size={20} className="mr-2 blur-[0.75px]" />Volunteer</p>
+              <p className="my-4 inline-flex"><Handshake size={20} className="mr-2 blur-[0.75px]" />Volunteer</p>
             </Link>
 
             <Link href="/tickets" className="light:text-black text-orange-200 hidden hover:text-orange-300 hover:scale-105 italic">
-              <p className="ml-3 my-4 inline-flex">Tickets</p>
+              <p className="my-4 inline-flex">Tickets</p>
             </Link>
 
             <button aria-label="Search" className="light:text-black text-orange-200 hover:text-orange-300 hover:scale-105 italic">
@@ -76,7 +76,7 @@ const Navbar = () => {
       </div>
 
       {isMobileMenuOpen && (
-        <div className="md:hidden bg-black shadow-md">
+        <div className="lg:hidden bg-black shadow-md">
           <div className="flex flex-col space-y-4 bg-black">
             <Link href="/aboutus" className="text-orange-200 hover:text-orange-300 hover:scale-99 bg-black italic" onClick={toggleMobileMenu}>
               <p className="ml-5 my-4 inline-flex"><ScrollIcon size={20} className="mr-3 blur-[0.75px]" /> About Us</p>
