ab40cc2de51a8e2c1ba33207b2378ff757c1d569
cloudflips32 <devadamstandish@gmail.com>
2025-06-22 18:00:28 -0400

    video w/l adjustments by viewpoint

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,18 +25,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-
     <html lang="en">
       <head>
         <script src="https://js-na2.hsforms.net/forms/embed/242985282.js"></script>
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500 h-auto lg:h-[100vh]`}>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-r from-purple-800 via-purple-400 to-blue-500 h-auto lg:h-[100vh] overflow-x-hidden`}>
         <video
           autoPlay
           loop
           muted
-          className="fixed -z-10 min-w-full h-[auto] md:h-[1080px] cover max-w-[1200px] md:max-w-[100vw] opacity-45"
+          className="fixed -z-10 min-w-full h-auto cover max-w-[1200px] md:max-w-[100vw] md:h-[870px] lg:h-[1080px] xl:h-[870px] 2xl:h-[1080px] opacity-45"
         >
           <source src="/video.mp4" type="video/mp4" />
         </video>
