ece9f31fcb091caae8b14acf137d6afba4dc3412
cloudflips32 <devadamstandish@gmail.com>
2025-07-04 11:54:34 -0400

    page-hero, workshop-form

--- src/app/workshops/page.jsx
+++ src/app/workshops/page.jsx
@@ -1,16 +1,17 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import Link from 'next/link';
+import PageHero from '../components/page-hero';
 
 export default function Workshops() {
 
   return (
     <>
       <main className="max-w-5xl mx-auto p-4 wrapper-7 mb-32">
-        <h1 className="text-4xl font-bold mt-20 mb-6 text-center text-teal-700">Workshops</h1>
+        <PageHero title="Speakers" subtitle="Meet our distinguished speakers for BSides SWFL 2025." />
         <p className='text-center text-amber-200 font-semibold md:mt-[30%] lg:mt-[22%] xl:mt-[15%] 2xl:mt-[22%]'>We are excited to announce our Call for Workshop Proposals for the upcoming BSides SWFL cybersecurity conference! We invite passionate professionals, educators, and enthusiasts to share their expertise by leading hands-on workshops that engage and inspire our diverse community. Whether you’re an experienced instructor or presenting for the first time, this is a great opportunity to help others learn new skills, explore emerging topics, and foster collaboration in a friendly, supportive environment. <Link className='text-teal-500 hover:text-teal-700' href='/contactus'>Submit your workshop idea</Link> and help us make this year’s event an unforgettable learning experience for all!</p>
 
         <div className="text-center mt-12">
-          <a href='/contactus'>
+          <a href='/workshops-form'>
             <button className="bg-teal-600 text-white font-bold py-3 px-8 rounded-lg shadow-md hover:bg-teal-700 transition-colors duration-200 w-full md:w-auto">
               Reach Out Today! <img className="inline-block w-12 h-12 ml-4" src="bsideslogo.png" />
             </button>
